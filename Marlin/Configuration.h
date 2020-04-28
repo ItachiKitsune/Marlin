db03d6cec0c814865b1c5be72192d01978e32d45
itachi <itachi@itachikitsune.com>
2020-04-27 21:49:30 -0400

    Added Linear Advance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -968,7 +968,6 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -49, -7, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1273,7 +1272,7 @@
 
     // Beyond the probed grid, continue the implied tilt?
     // Default is to maintain the height of the nearest edge.
-    //#define EXTRAPOLATE_BEYOND_GRID
+    #define EXTRAPOLATE_BEYOND_GRID
 
     //
     // Experimental Subdivision of the grid by Catmull-Rom method.
